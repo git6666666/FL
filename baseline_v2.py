6f86317d106ca3d303e5cb86113c0297537d8317
git6666666 <93507581+git6666666@users.noreply.github.com>
2022-02-23 17:10:08 +0800

    Create baseline_v2.py

---
+++ baseline_v2.py
@@ -0,0 +1,117 @@
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+
+#from utils import get_dataset
+from torchvision import datasets, transforms
+from mlp import MLP
+
+def test_inference(model, test_dataset):
+    """ Returns the test accuracy and loss.
+    """
+
+    model.eval()
+    loss, total, correct = 0.0, 0.0, 0.0
+
+    device = 'cpu'
+    criterion = nn.NLLLoss().to(device)
+    testloader = DataLoader(test_dataset, batch_size=128,
+                            shuffle=False)
+
+    for batch_idx, (images, labels) in enumerate(testloader):
+        images, labels = images.to(device), labels.to(device)
+
+        # Inference
+        outputs = model(images)
+        batch_loss = criterion(outputs, labels)
+        loss += batch_loss.item()
+
+        # Prediction
+        _, pred_labels = torch.max(outputs, 1)
+        pred_labels = pred_labels.view(-1)
+        correct += torch.sum(torch.eq(pred_labels, labels)).item()
+        total += len(labels)
+
+    accuracy = correct/total
+    return accuracy, loss
+
+if __name__ == '__main__':
+    
+
+    device = 'cpu'
+    # load datasets
+    
+    train_dataset = datasets.MNIST('./data/mnist/', train=True, download=True,
+                   transform=transforms.Compose([
+                       transforms.ToTensor(),
+                       transforms.Normalize((0.1307,), (0.3081,))
+                   ]))
+    test_dataset = datasets.MNIST('./data/mnist/', train=False, download=True,
+                   transform=transforms.Compose([
+                       transforms.ToTensor(),
+                       transforms.Normalize((0.1307,), (0.3081,))
+                   ]))
+    
+
+    # BUILD MODEL
+    # Multi-layer preceptron
+    img_size = train_dataset[0][0].shape
+    len_in = 1
+    for x in img_size:
+            len_in *= x
+            global_model = MLP(dim_in=len_in, dim_hidden=64,
+                               dim_out=10)
+
+
+    # Set the model to train and send it to device.
+    global_model.to('cpu')
+    global_model.train()
+    print(global_model)
+
+    # Training
+    # Set optimizer and criterion
+
+    optimizer = torch.optim.SGD(global_model.parameters(), lr=0.01,
+                                    momentum=0.5)
+
+
+    trainloader = DataLoader(train_dataset, batch_size=64, shuffle=True)
+    criterion = torch.nn.NLLLoss().to(device)
+    epoch_loss = []
+
+    for epoch in tqdm(range(10)):
+        batch_loss = []
+
+        for batch_idx, (images, labels) in enumerate(trainloader):
+            images, labels = images.to(device), labels.to(device)
+
+            optimizer.zero_grad()
+            outputs = global_model(images)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            if batch_idx % 50 == 0:
+                print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                    epoch+1, batch_idx * len(images), len(trainloader.dataset),
+                    100. * batch_idx / len(trainloader), loss.item()))
+            batch_loss.append(loss.item())
+
+        loss_avg = sum(batch_loss)/len(batch_loss)
+        print('\nTrain loss:', loss_avg)
+        epoch_loss.append(loss_avg)
+
+    # Plot loss
+    plt.figure()
+    plt.plot(range(len(epoch_loss)), epoch_loss)
+    plt.xlabel('epochs')
+    plt.ylabel('Train loss')
+    plt.savefig('../nn_{}_{}_{}.png'.format('Mnist', 'MLP',
+                                                 10))
+    
+    # testing
+    test_acc, test_loss = test_inference(global_model, test_dataset)
+    print('Test on', len(test_dataset), 'samples')
+    print("Test Accuracy: {:.2f}%".format(100*test_acc))
